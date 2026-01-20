e0da83beca81d8ae857afa82078dc691871302ad
Jorge Manuel Figueroa Perez <jfigueroa@cisneros.com>
2026-01-20 15:46:45 -0500

    add reset password modal component for user password recovery

---
+++ resources/views/components/⚡password-reset/password-reset.blade.php
@@ -0,0 +1,36 @@
+<div class="modal fade" id="resetPasswordModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="resetPasswordModalLabel" aria-hidden="true" wire:ignore.self>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="resetPasswordModalLabel">Reset Password</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form wire:submit.prevent="resetPassword" id="resetPasswordForm">
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input wire:model="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" id="password" name="password">
+                        @error('password')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="password_confirmation" class="form-label">Confirm Password</label>
+                        <input wire:model="password_confirmation" type="password"
+                            class="form-control @error('password_confirmation') is-invalid @enderror"
+                            id="password_confirmation" name="password_confirmation">
+                        @error('password_confirmation')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary" form="resetPasswordForm">Send</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
