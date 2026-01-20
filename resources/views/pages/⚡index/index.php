43d3533080f5b768edaf9230f9ef91871b4738eb
Jorge Manuel Figueroa Perez <jfigueroa@cisneros.com>
2026-01-20 15:45:58 -0500

    add computed properties for car statistics in index component

--- resources/views/pages/⚡index/index.php
+++ resources/views/pages/⚡index/index.php
@@ -1,8 +1,25 @@
 <?php
 
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 new #[Layout('layouts::app')] class extends Component {
-    //
+    #[Computed]
+    public function carsOnSale()
+    {
+        return rand(10, 100);
+    }
+
+    #[Computed]
+    public function carsSold()
+    {
+        return rand(100, 1000);
+    }
+
+    #[Computed]
+    public function avgCarPrice()
+    {
+        return rand(20000, 100000);
+    }
 };
